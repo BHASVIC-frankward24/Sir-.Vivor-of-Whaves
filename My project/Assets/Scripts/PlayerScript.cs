763f055a8b6ab3a311a55c2e7768f582b0c00575
Frank ward <frank.ward24@students.bhasvic.ac.uk>
2025-10-06 15:31:26 +0100

    Update to the movement code for testing

--- My project/Assets/Scripts/PlayerScript.cs
+++ My project/Assets/Scripts/PlayerScript.cs
@@ -1,39 +1,30 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class Playerscript : MonoBehaviour
 {
 
-    private float movementForce;
-    private Rigidbody2D myRigidBody2D;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        movementForce = 50;
+
     }
         
     
     // Update is called once per frame
     public void Update(){
-        float v = 0;
-        float h = 0;
-
-
-        if (UnityEngine.Input.GetKey(KeyCode.W) && !UnityEngine.Input.GetKey(KeyCode.S))
+        float horizontal = 0f;
+        if(Keyboard.current.leftArrowKey.isPressed)
         {
-            v = 1;
+            horizontal = -1.0f;
         }
-        if (UnityEngine.Input.GetKey(KeyCode.S) && !UnityEngine.Input.GetKey(KeyCode.W)){
-            v = -1;
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
         }
-
-
-        //float h = Input.GetAxis("Horizontal");
-        //float v = Input.GetAxis("Vertical");
-        Vector3 tempVect = new Vector3(h, v, 0);
-        tempVect = tempVect.normalized * movementForce * Time.deltaTime;
-        myRigidBody2D.MovePosition(myRigidBody2D.transform.position + tempVect);
+            Debug.Log(horizontal);
     }
 
 }
