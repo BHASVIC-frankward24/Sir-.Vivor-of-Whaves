136d6abbed9e38d8e09aa2f8cfcf140f06296b5a
BHASVIC-frankward24 <frank.ward24@students.bhasvic.ac.uk>
2025-10-13 15:31:01 +0100

    Updated the enemy movement code
    - There is an error where the enemy can't move up or to the right.

--- My project/Assets/Scripts/EnemyMovement.cs
+++ My project/Assets/Scripts/EnemyMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private float VelocityMultiplier = 3;
     private GameObject playerObj = null;
+    private float xCoord;
+    private float yCoord;
+    private float denominator;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,27 +22,32 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        velocityX = 0f;
-        velocityY = 0f;
-
-        if (playerObj == null)
-            playerObj = GameObject.FindGameObjectWithTag("Player");
-
-        float xCoord = playerObj.transform.position.x;
-        float yCoord = playerObj.transform.position.x;
-        if (transform.position.y > yCoord)
-            velocityY = -1f;
-
-        else if (transform.position.y < yCoord)
-            velocityY = 1f;
-
-        if (transform.position.x > xCoord)
-            velocityX = -1f;
-
-        else if (transform.position.x < xCoord)
-            velocityX = 1f;
+        
+        velocityX = FindMovementRatio('x');
+        velocityY = FindMovementRatio('y');
         
         transform.position += new UnityEngine.Vector3(velocityX, velocityY, 0f) * VelocityMultiplier * Time.deltaTime;
         
+    }
+
+    float FindMovementRatio(char axis){
+        if (playerObj == null)
+            playerObj = GameObject.FindGameObjectWithTag("Player");
+
+        xCoord = transform.position.x - playerObj.transform.position.x;
+        yCoord = transform.position.y - playerObj.transform.position.y;
+
+        denominator = Mathf.Sqrt(xCoord*xCoord + yCoord*yCoord); // Calculates what the length of the direct line between the player and enemy is.
+        if(axis == 'x' && transform.position.x < playerObj.transform.position.x)
+            return (xCoord / denominator);
+        else if(axis == 'x' && transform.position.x > playerObj.transform.position.x)
+            return (-1 * xCoord / denominator);
+
+        if(axis == 'y' && transform.position.y < playerObj.transform.position.y)
+            return (yCoord / denominator);
+        else if(axis == 'y' && transform.position.y > playerObj.transform.position.y)
+            return (-1 * yCoord / denominator);
+        
+        return 0f;
     }
 }
