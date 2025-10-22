06552f70731107fe70cef7a821e8ab11fcc3bd38
BHASVIC-frankward24 <frank.ward24@students.bhasvic.ac.uk>
2025-10-22 11:43:25 +0100

    Added a pause menu
    next:
    - Do the write up for the pause menu and its code

---
+++ My project/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void onPress()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
