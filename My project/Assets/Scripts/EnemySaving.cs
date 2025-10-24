ef3de8902c788097fb3c2e5819d7b751160c15c7
BHASVIC-frankward24 <frank.ward24@students.bhasvic.ac.uk>
2025-10-24 09:51:43 +0100

    Added a saving system for the enemy and finished the analysis work for it

---
+++ My project/Assets/Scripts/EnemySaving.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemySaving : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        LoadEnemyData();
+    }
+
+    // Update is called once per frame
+    public void SaveEnemyData()
+    {
+        PlayerPrefs.SetFloat("EVIL XCoord", transform.position.x);
+        PlayerPrefs.SetFloat("EVIL YCoord", transform.position.y);
+    }
+
+    public void LoadEnemyData()
+    {
+        float X = PlayerPrefs.GetFloat("EVIL XCoord", 0);
+        float Y = PlayerPrefs.GetFloat("EVIL YCoord", 0);
+        transform.position = new UnityEngine.Vector3(X, Y, 0f);
+    }
+
+    public void ResetEnemyData()
+    {
+        transform.position = new UnityEngine.Vector3(2.000811f, -0.7526299f, -1f);
+        SaveEnemyData();
+    }
+}
--- My project/Assets/Scripts/PauseButton.cs
+++ My project/Assets/Scripts/PauseButton.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PauseButton : MonoBehaviour
 {
     private GameObject playerObj = null;
+    private GameObject EVILObj = null;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OnPress()
     {
@@ -10,6 +11,11 @@ public class PauseButton : MonoBehaviour
             playerObj = GameObject.FindGameObjectWithTag("Player");
 
         playerObj.GetComponent<Playerscript>().SavePlayerData();
+
+        if (EVILObj == null)
+            EVILObj = GameObject.FindGameObjectWithTag("EVIL");
+
+        EVILObj.GetComponent<EnemySaving>().SaveEnemyData();
 
         UnityEngine.SceneManagement.SceneManager.LoadScene("Pause menu");
     }
